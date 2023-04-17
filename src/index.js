5937290df6428cbc98bd5cba5a3c6aa6bb2dce39
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-04-17 14:08:17 +0200

    dispatch my first action

--- src/index.js
+++ src/index.js
@@ -2,7 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+const counter = (state = 0, action) => {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  }
+  if (action.type === 'DECREMENT') {
+    return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(increment());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +36,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
